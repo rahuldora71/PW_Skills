f1cb23fead0289d4528acfc9622e98d500959357
rahuldora71 <rahuldoraksp02@gmail.com>
2023-11-04 21:44:08 +0530

    name rahul  2

--- src/Loops_1.java
+++ src/Loops_1.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Loops_1 {
+    public int num(int a){
+        System.out.println(a);
+        return a;
+    }
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
         int ram=sc.nextInt();
--- src/new1.java
+++ src/new1.java
@@ -1,2 +1,6 @@
 public class new1 {
+    public static void main(String[] args) {
+        Loops_1 loops1=new Loops_1();
+        loops1.num(34323);
+    }
 }
