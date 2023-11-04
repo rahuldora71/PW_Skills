6ff8d7eaebf3ab315dd48d6029061998f32ed115
rahuldora71 <rahuldoraksp02@gmail.com>
2023-11-04 21:36:27 +0530

    name rahul

---
+++ src/Loops_1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Loops_1 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int ram=sc.nextInt();
+        System.out.println(ram);
+        System.out.println("Hello World");
+
+
+    }
+}
